268289aecf99bed1f4395e31099db1932a33b640
tnktks111 <tnk-tsks17074@g.ecc.u-tokyo.ac.jp>
2025-03-20 09:58:05 +0900

    first

---
+++ C09/ex01/includes/ft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttanaka <ttanaka@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/20 01:01:08 by ttanaka           #+#    #+#             */
+/*   Updated: 2025/03/20 01:01:09 by ttanaka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int 	ft_strcmp(char *s1, char *s2);
+
+#endif
